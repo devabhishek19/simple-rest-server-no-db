0c4f92561393fdf0842186bf059e42193696071c
Abhishek Patel <dev.abhishek19@gmail.com>
2015-10-20 17:09:36 -0400

    some changes

--- public/Routingtest/app.js
+++ public/Routingtest/app.js
@@ -1,18 +1,18 @@
-var testingApp = angular.module('testingApp', ['ui-router'])
+var testingApp = angular.module('testingApp', ['ui.router'])
 .config([$urlRouterProvider, $stateProvider, function($urlRouterProvider, $stateProvider){
 	$stateProvider
 		.state('home', {
 			url: '/',
-			templateUrl: 'templates/home.html'
+			templateUrl: 'home.html'
 		})
 		.state('home.detail',{
 			url: '/home.detail',
-			templateUrl: 'templates/home_detail.html'
+			templateUrl: '/templates/home_detail.html'
 		})
 		.state('product',{
 			url: '/products',
-			templateUrl: 'templates/products.html'
+			templateUrl: '/products.html'
 		})
 
-	$urlRouterProvider('', '/');
-}])
+	$urlRouterProvider.when('', '/');
+}]);
