f1382717f26b3d91be48378aca3f615e999b5291
Abhishek Patel <dev.abhishek19@gmail.com>
2015-10-22 11:15:05 -0400

    some

---
+++ public/Routingtest/app.js
@@ -0,0 +1,16 @@
+angular.module('app', []).
+	config(function($routeProvider){
+		$routeProvider.
+			when('/',{
+				template: "home.html"
+			}).
+			when('/about',{ template: "about.html"}).
+			when('/team', { template: "team.html"}).
+			when('/contact', { tempate: "contact.html"}).
+			otherwise({ redirectTo : '/'
+			})
+	});
+
+	function mainctlr($scope){
+
+	}
